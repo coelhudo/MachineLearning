d53fd06e92caeff194b6c99aad9372608b182cb6
coelhudo <coelhudo@gmail.com>
2015-02-19 00:08:29 -0600

    Programming questions week 2 - finished

--- mlclass-ex1-008/mlclass-ex1/gradientDescent.m
+++ mlclass-ex1-008/mlclass-ex1/gradientDescent.m
@@ -1,33 +1,37 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
-%GRADIENTDESCENT Performs gradient descent to learn theta
+	     %GRADIENTDESCENT Performs gradient descent to learn theta
 %   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
 % Initialize some useful values
-m = length(y); % number of training examples
-J_history = zeros(num_iters, 1);
+  m = length(y); % number of training examples
+  function [cost] = currentCost(currentTheta,Xj)
+    cost = alpha * sum((X*currentTheta - y).*Xj) / m;
+  end
+  J_history = zeros(num_iters, 1);
+  for iter = 1:num_iters
 
-for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
+ % ====================== YOUR CODE HERE ======================
+% Instructions: Perform a single gradient step on the parameter vector
+%               theta. 
+%
+% Hint: While debugging, it can be useful to print out the values
+%       of the cost function (computeCost) and gradient here.
+%
 
 
 
-    % ============================================================
 
-    % Save the cost J in every iteration    
+
+
+
+	% ============================================================
+
+			      % Save the cost J in every iteration
     J_history(iter) = computeCost(X, y, theta);
-
-end
+    tempTheta1 = theta - currentCost(theta,X(:,1));
+    tempTheta2 = theta - currentCost(theta,X(:,2));
+    theta = [tempTheta1(1,1); tempTheta2(2,1)]
+  end
 
 end
