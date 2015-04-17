e806a351c7cab41d8d3e1214001e7e072af83418
coelhudo <coelhudo@gmail.com>
2015-04-17 14:09:22 -0400

    All remaining exercises

--- questions/mlclass-ex8-008/mlclass-ex8/cofiCostFunc.m
+++ questions/mlclass-ex8-008/mlclass-ex8/cofiCostFunc.m
@@ -6,7 +6,7 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 %   collaborative filtering problem.
 %
 
-% Unfold the U and W matrices from params
+			     % Unfold the U and W matrices from params
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
 Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
@@ -40,13 +40,16 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-costPerUnit = ((X * Theta') - Y).^2;
+costPerUnit = ((X * Theta') - Y); %I know, terrible name
 costPerUnit = costPerUnit .* R;
-J = sum(costPerUnit(:)) / 2;
 
+J = (sum(costPerUnit(:).^2) + lambda * sum(sum(Theta.^2)) + lambda * sum(sum(X.^2)))/2;
+
+X_grad = costPerUnit * Theta + lambda * X;
+
+Theta_grad = costPerUnit' * X + lambda * Theta;
 
 % =============================================================
-
 grad = [X_grad(:); Theta_grad(:)];
 
 end
